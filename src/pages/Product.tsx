023d734df73c3b4b608ad2086756449b5cf48c02
Jeremy Gordillo <jere.gordi@gmail.com>
2022-01-18 18:59:29 +0100

    Minor improvements to Product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ProductView from "../components/Product/ProductView";
+import ProductDetails from "../components/Product/ProductDetails";
 import ProductService from "../service/ProductService";
 import { Product } from "../types";
 
 const ProductPage = () => {
   const { id } = useParams();
   const [product, setProduct] = useState<Product>();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (id) ProductService.getProduct(id).then((res) => setProduct(res));
+    if (id) {
+      setLoading(true);
+      ProductService.getProduct(id)
+        .then((res) => setProduct(res))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }
   }, [id]);
 
-  return product ? <ProductView {...product} /> : null;
+  if (loading) return <p>Loading...</p>;
+  return product ? <ProductDetails {...product} /> : null;
 };
 
 export default ProductPage;
